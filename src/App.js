0c929a3cc982eb34d3ba287e0b92d76cd3eefc73
Nynke Vollema <46654246+NynkeVollema@users.noreply.github.com>
2021-04-05 16:24:00 +0200

    Add dummy line to homepage

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          My awesome GitHub Page running using React
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
